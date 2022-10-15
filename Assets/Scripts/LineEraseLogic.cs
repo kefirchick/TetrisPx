770e4a7c370f6d1710a17761679b86ee367432f2
Пользователь <i.n.kustov@gmail.com>
2022-10-15 17:10:35 +0600

    LineErase refactoring

--- Assets/Scripts/LineEraseLogic.cs
+++ Assets/Scripts/LineEraseLogic.cs
@@ -5,24 +5,18 @@ using UnityEngine;
 public class LineEraseLogic : MonoBehaviour
 {
     public GameObject[] arrayOfBlocks;
-
     public AudioSource eraseSound;
-
     public ParticleSystem explosion;
 
     private ParticleSystem thisExplosion;
-
     private Color blockColor;
-
     private float checkY;
     private float lineLuft = 0.3f;
-    
     private int lineCount;
     
     // Update is called once per frame
     void Update()
     {
-        // Erase compleated lines
         arrayOfBlocks = GameObject.FindGameObjectsWithTag("Block");
         for (float y = -19.5f; y < 0f; y++)
         {
@@ -37,21 +31,25 @@ public class LineEraseLogic : MonoBehaviour
             }
             if (lineCount > 9)
             {
-                this.GetComponent<AudioSource>().pitch = Random.Range(0.7f, 1.4f);
-                eraseSound.Play();
-                foreach (GameObject destroyBlocks in arrayOfBlocks)
-                {
-                    checkY = destroyBlocks.transform.localPosition.y;
-                    if ((checkY > y - lineLuft) && (checkY < y + lineLuft))
-                    {
-                        blockColor = destroyBlocks.GetComponent<SpriteRenderer>().color;
-                        thisExplosion = Instantiate(explosion, destroyBlocks.transform.position, Quaternion.identity);
-                        var main = thisExplosion.main;
-                        main.startColor = blockColor;
-                        Destroy(destroyBlocks);
-                        UILogic.instance.UpScore();
-                    }
-                }
+                eraseLine(y);
+            }
+        }
+    }
+
+    void eraseLine(float y) {
+        this.GetComponent<AudioSource>().pitch = Random.Range(0.7f, 1.4f);
+        eraseSound.Play();
+        foreach (GameObject destroyBlocks in arrayOfBlocks)
+        {
+            checkY = destroyBlocks.transform.localPosition.y;
+            if ((checkY > y - lineLuft) && (checkY < y + lineLuft))
+            {
+                blockColor = destroyBlocks.GetComponent<SpriteRenderer>().color;
+                thisExplosion = Instantiate(explosion, destroyBlocks.transform.position, Quaternion.identity);
+                var main = thisExplosion.main;
+                main.startColor = blockColor;
+                Destroy(destroyBlocks);
+                UILogic.instance.UpScore();
             }
         }
     }
