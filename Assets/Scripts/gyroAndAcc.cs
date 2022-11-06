d593b7b60407bb08ece214ca5f9a55710d337086
Пользователь <i.n.kustov@gmail.com>
2022-11-06 20:18:24 +0600

    gyro check

--- Assets/Scripts/gyroAndAcc.cs
+++ Assets/Scripts/gyroAndAcc.cs
@@ -11,17 +11,21 @@ public class gyroAndAcc : MonoBehaviour
 
     void Start()
     {
-        m_Gyro = Input.gyro;
-        m_Gyro.enabled = true;
+        if (SystemInfo.supportsGyroscope)
+        {
+            m_Gyro = Input.gyro;
+            m_Gyro.enabled = true;
+        }
     }
 
     void Update()
     {
-        zAngle = (Input.gyro.attitude.eulerAngles.z - 90) * Mathf.PI / 180;
-        xGravity = -10f * Mathf.Sin(zAngle);
-        yGravity = -10f * Mathf.Cos(zAngle);
-        Physics2D.gravity = new Vector2(xGravity, yGravity);
-        // Debug.Log($"{Input.gyro.attitude.eulerAngles.z}    {zAngle}    {xGravity}    {yGravity}");
-        // Debug.Log($"{Input.acceleration.x}    {Input.acceleration.y}");
+        if (SystemInfo.supportsGyroscope) 
+        {
+            zAngle = (Input.gyro.attitude.eulerAngles.z - 90) * Mathf.PI / 180;
+            xGravity = -10f * Mathf.Sin(zAngle);
+            yGravity = -10f * Mathf.Cos(zAngle);
+            Physics2D.gravity = new Vector2(xGravity, yGravity);
+        }
     }
 }
